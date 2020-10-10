0625bee29925108acede9c240a5b65a87e511e93
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-10-10 14:43:57 -0300

    cria opcao menu

--- main.py
+++ main.py
@@ -4,16 +4,34 @@ import inspect
 def inicio():
     '''Apresenta programa'''
 
-    Alfredo.normal()
-    resposta = input('Olá esse aqui é o Alfredo. \n ')
+    Alfredo.linguinha()
+    print('Olá esse aqui é o Alfredo. \n ')
 
 
 
 def menu():
-    resposta = input('Olá esse aqui é o Alfredo. \n ')
+
+    resposta = input('Escolha entre, feliz, assustado, triste, linguinha. \n Escreva "sair" para terminar')
+    return resposta.lower()
 
 if __name__ == '__main__':
     inicio()
+    opcao = menu()
+
+    while (opcao != "sair"):
+
+        if(opcao == 'feliz'):
+            Alfredo.feliz()
+        elif(opcao == 'assustado'):
+            Alfredo.assustado()
+        elif(opcao == 'triste'):
+            Alfredo.triste()
+        elif(opcao == 'linguinha'):
+            print('VC ESCOLHEU O MAIS BRABO')
+            Alfredo.linguinha()
+
+        opcao = menu()
+
 
 
 
