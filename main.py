3a01a443e71f55d37717bb9058cc48dc26d46b63
Lucas Lange Barrozo <lucas.barrozo@radixeng.com.br>
2020-10-10 14:28:25 -0300

    começa projeto alfredo

---
+++ main.py
@@ -0,0 +1,19 @@
+import Alfredo
+import inspect
+
+def inicio():
+    '''Apresenta programa'''
+
+    Alfredo.normal()
+    resposta = input('Olá esse aqui é o Alfredo. \n ')
+
+
+
+def menu():
+    resposta = input('Olá esse aqui é o Alfredo. \n ')
+
+if __name__ == '__main__':
+    inicio()
+
+
+
